446690ce4083881b90a0c9787d69b9268fc3c96a
NekoDar <neko-d-05@mail.ru>
2024-02-20 11:33:55 +0300

    add load and save for PlayerManager

--- player/player_manager.py
+++ player/player_manager.py
@@ -1,3 +1,34 @@
+import os
+import json
+
 class PlayerManager:
     def __init__(self):
-        pass
+        self.path = os.path.join(os.getcwd(), '..', 'data', 'players', 'player_data.json')
+    
+    def save_file(self):
+        """
+        Сохранение данных о игроке
+        
+        Args:
+            None
+        
+        Returns:
+            None
+        """
+        with open(self.path, 'w') as file:
+            json.dump(self.data, file, indent=4)
+    
+    
+    def load_file(self):
+        """
+        Получение расы игрока
+        
+        Args:
+            None
+        
+        Returns:
+            data (DataFiles): Загруженный файл данных
+        """
+        with open(self.path, 'r') as file:
+            return json.load(file)
+    
