34eb7c3401d7e33aa23b17cc9dc8a9cc569e8b06
NekoDar <neko-d-05@mail.ru>
2024-02-28 03:28:01 +0300

    add tests for tag_system

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import asyncio
 
 from tests.test_file_work import test_FileWork
+from tests.test_tag_system import test_TagData, test_TagsManager
 
 
 async def run_tests():
     await test_FileWork()
+    await test_TagData()
+    await test_TagsManager()
+
 async def main():
     pass
 
---
+++ tests/test_tag_system.py
@@ -0,0 +1,65 @@
+from data.tags_data import TagData
+from tags_manager import TagsManager
+
+
+async def test_TagData():
+    try:
+        tag_data_1 = TagData("test_id")
+        tag_data_2 = TagData()  # Проверяем создание объекта без идентификатора
+    except Exception as err:
+        print(err)
+        return False
+    
+    # Проверяем, что идентификаторы установлены правильно
+    if tag_data_1.get_id() == "test_id" and tag_data_2.get_id() is None:
+        print("TagData test successful.")
+        return True
+    else:
+        print("TagData test failed.")
+        return False
+
+async def test_TagsManager():
+    try:
+        tags_manager = TagsManager()
+    except Exception as err:
+        print(err)
+        return False
+    
+    # Создаем несколько объектов TagData для тестирования
+    tag_data_1 = TagData("tag1")
+    tag_data_2 = TagData("tag2")
+    tag_data_3 = TagData("tag3")
+    
+    # Создаем список тегов и добавляем теги
+    tags_list = []
+    tags_manager.add(tags_list, tag_data_1)
+    tags_manager.add(tags_list, tag_data_2)
+    
+    # Проверяем, что теги успешно добавлены
+    if len(tags_list) == 2 and tag_data_1 in tags_list and tag_data_2 in tags_list:
+        print("TagsManager add test successful.")
+    else:
+        print("TagsManager add test failed.")
+        return False
+    
+    # Удаляем один из тегов
+    tags_manager.rm(tags_list, tag_data_2)
+    
+    # Проверяем, что тег успешно удален
+    if len(tags_list) == 1 and tag_data_2 not in tags_list:
+        print("TagsManager rm test successful.")
+    else:
+        print("TagsManager rm test failed.")
+        return False
+    
+    # Сортируем список тегов
+    tags_manager.sort_arr(tags_list)
+    
+    # Проверяем, что список тегов отсортирован по идентификаторам
+    sorted_ids = [tag.get_id() for tag in tags_list]
+    if sorted_ids == sorted(sorted_ids):
+        print("TagsManager sort_arr test successful.")
+        return True
+    else:
+        print("TagsManager sort_arr test failed.")
+        return False
