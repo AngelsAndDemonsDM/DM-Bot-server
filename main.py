0614c2b4b292aa787daf66e6bfb20196ba4df968
NekoDar <neko-d-05@mail.ru>
2024-02-28 03:27:23 +0300

    Обновление теста для FileWork

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import asyncio
 
-from tests.test_file_work import TestFileWorkDef
+from tests.test_file_work import test_FileWork
 
 
+async def run_tests():
+    await test_FileWork()
 async def main():
-    await TestFileWorkDef()
+    pass
 
 if __name__ == "__main__":
 	asyncio.run(main())
--- tests/test_file_work.py
+++ tests/test_file_work.py
@@ -1,3 +1,5 @@
+import os
+
 from abstract.file_work import FileWork
 
 
@@ -5,18 +7,39 @@ class TestFileWork(FileWork):
     def __init__(self):
         super().__init__("test/test_file_work.dat")
 
-async def TestFileWorkDef():
+async def test_FileWork():
     try:
-        class_file_work_d = FileWork("test/test_file_work.dat")
+        class_file_work = FileWork("test/test_file_work.dat")
     except Exception as err:
         print(err)
+
+    # Создаем экземпляр класса TestFileWork
+    file_work = TestFileWork()
     
-    class_file_work = TestFileWork()
-    await class_file_work.create_file()    
-    class_file_work.data = {"key": "value"}
-    await class_file_work.save_data()
+    # Создаем файл и директорию
+    await file_work.create_file()
     
-    loaded_data = await class_file_work.load_data()
-    print(loaded_data)
+    # Проверяем, что файл был создан
+    file_path = os.path.join(os.getcwd(), 'data', 'test', 'test_file_work.dat')
+    if os.path.exists(file_path):
+        print("File successfully created.")
+    else:
+        print("Failed to create file.")
+        return False
     
-    return True
+    # Устанавливаем данные
+    file_work.data = {"key": "value"}
+    
+    # Сохраняем данные
+    await file_work.save_data()
+    
+    # Загружаем данные
+    loaded_data = await file_work.load_data()
+
+    # Проверяем, что загруженные данные соответствуют ожидаемым
+    if loaded_data == {"key": "value"}:
+        print("FileWork test successful.")
+        return True
+    else:
+        print("FileWork test failed.")
+        return False
