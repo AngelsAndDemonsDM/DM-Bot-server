43c91e75d614cbf57e0999bfc08a6c9b80f7f5c3
NekoDar <neko-d-05@mail.ru>
2024-02-28 03:46:24 +0300

    Минорное исправление run_tests
    Теперь он чистит за собой папки

--- main.py
+++ main.py
@@ -6,29 +6,35 @@ from tests.test_file_work import test_FileWork
 from tests.test_tag_system import test_TagData, test_TagsManager
 
 
+def del_test_folder():
+    test_folder_path = os.path.join(os.getcwd(), 'data', 'test')
+    if os.path.exists(test_folder_path):
+        shutil.rmtree(test_folder_path)
+        print("Test folder deleted successfully.")    
+
 async def run_tests():
     """
     Запускает тесты из папки tests.
 
-    Перед запуском каждого теста проверяет, существует ли папка 'test' в папке 'data'.
+    Перед запуском тестов проверяет, существует ли папка 'test' в папке 'data'.
+    Если папка существует, она удаляется.
+
+    После запуска тестов проверяетб, существует ли папка 'test' в папке 'data'.
     Если папка существует, она удаляется.
 
     Returns:
         bool: True, если все тесты успешно пройдены, в противном случае False.
     """
-    test_folder_path = os.path.join(os.getcwd(), 'data', 'test')
-    if os.path.exists(test_folder_path):
-        shutil.rmtree(test_folder_path)
-        print("Test folder deleted successfully.")
-    else:
-        print("Test folder does not exist.")
-
+    del_test_folder()
+    
     # Запускаем тесты
     all_tests_passed = True
     all_tests_passed &= await test_FileWork()
     all_tests_passed &= await test_TagData()
     all_tests_passed &= await test_TagsManager()
-
+    
+    del_test_folder()
+    
     return all_tests_passed
 
 
