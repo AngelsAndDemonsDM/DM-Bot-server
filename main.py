42989c5f37594154ee480f1357a47bc4ba541c41
NekoDar <neko-d-05@mail.ru>
2024-02-28 03:35:52 +0300

    Обновление функции run_tests()

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 import asyncio
+import os
+import shutil
 
 from tests.test_file_work import test_FileWork
 from tests.test_tag_system import test_TagData, test_TagsManager
 
 
 async def run_tests():
-    await test_FileWork()
-    await test_TagData()
-    await test_TagsManager()
+    """
+    Запускает тесты из папки tests.
+
+    Перед запуском каждого теста проверяет, существует ли папка 'test' в папке 'data'.
+    Если папка существует, она удаляется.
+
+    Returns:
+        bool: True, если все тесты успешно пройдены, в противном случае False.
+    """
+    test_folder_path = os.path.join(os.getcwd(), 'data', 'test')
+    if os.path.exists(test_folder_path):
+        shutil.rmtree(test_folder_path)
+        print("Test folder deleted successfully.")
+    else:
+        print("Test folder does not exist.")
+
+    # Запускаем тесты
+    all_tests_passed = True
+    all_tests_passed &= await test_FileWork()
+    all_tests_passed &= await test_TagData()
+    all_tests_passed &= await test_TagsManager()
+
+    return all_tests_passed
+
 
 async def main():
-    pass
+    test_result = await run_tests()
+    print("All tests passed:", test_result)
+
 
 if __name__ == "__main__":
-	asyncio.run(main())
+    asyncio.run(main())
