843691ed35ab15f69b0737c80c4a63f9c19527eb
unknown <neko-d-05@mail.ru>
2024-03-22 01:19:17 +0300

    Повторная чистка кода тегов

--- Code.DM-Bot/etc/tag.py
+++ Code.DM-Bot/etc/tag.py
@@ -2,23 +2,23 @@ from typing import Union
 
 
 class Tag:
-    def __init__(self, id=None):
+    def __init__(self, id: str):
         """
         Инициализация объекта Tag.
 
         Args:
-            id (string, optional): Идентификатор тега. По умолчанию None.
-        
-        Raises:
-            TypeError: Если тип переданного идентификатора не поддерживается.
+            id (str): Идентификатор тега.
         """
-        if id is None:
-            self.id = None
-        elif not self.set_id(id):
-            raise TypeError(f"Tag id in {self.__class__.__name__}. must be a `string` type.")
+        self._id = id
     
     def __str__(self):
-        return self.id
+        """
+        Возвращает строковое представление тега.
+
+        Returns:
+            str: Строковое представление тега.
+        """
+        return self._id
 
     # Get методы
     @property
@@ -27,30 +27,30 @@ class Tag:
         Получение ID тега.
 
         Returns:
-            id (string): ID тега.
+            str: ID тега.
         """
-        return self.id
+        return self._id
 
     # Set методы
     @id.setter
-    def set_id(self, id: Union[str, int, float]):
+    def id(self, id: Union[str, int, float]):
         """
         Установка ID тега.
 
         Args:
-            id (string): ID тега.
+            id (str, int, float): Новый идентификатор тега.
         """
-        self.id = str(id)
+        self._id = str(id)
 
 class TagsManager:
     def find(self, arr_tags: list, tag: str):
         """
         Ищет тег в списке тегов.
-        
+
         Args:
             arr_tags (list): Список тегов для поиска.
-            tag (Tag or str or int or float): Тег для поиска. Может быть объектом класса Tag или его идентификатором.
-        
+            tag (str): Тег для поиска.
+
         Returns:
             bool: True, если тег найден, в противном случае False.
         """
@@ -63,10 +63,13 @@ class TagsManager:
     def add(self, arr_tags: list, tag: Tag):
         """
         Добавляет тег в список тегов.
-        
+
         Args:
             arr_tags (list): Список тегов, в который нужно добавить тег.
-            tag (Tag or str or int or float): Тег для добавления. Может быть объектом класса Tag или его идентификатором.
+            tag (Tag): Тег для добавления.
+
+        Returns:
+            bool: True, если тег успешно добавлен, в противном случае False.
         """
         if not self.find(arr_tags, tag):
             arr_tags.append(tag)
@@ -77,11 +80,11 @@ class TagsManager:
     def remove(self, arr_tags: list, tag: Tag):
         """
         Удаляет тег из списка тегов.
-        
+
         Args:
             arr_tags (list): Список тегов, из которого нужно удалить тег.
-            tag (Tag or str or int or float): Тег для удаления. Может быть объектом класса Tag или его идентификатором.
-        
+            tag (Tag): Тег для удаления.
+
         Returns:
             bool: True, если тег успешно удален, в противном случае False.
         """
@@ -94,8 +97,8 @@ class TagsManager:
     def sort_arr(self, arr_tags: list):
         """
         Сортирует список тегов по их идентификаторам.
-        
+
         Args:
             arr_tags (list): Список тегов для сортировки.
         """
-        arr_tags.sort(key=lambda x: x.get_id())
+        arr_tags.sort(key=lambda x: x.id)
