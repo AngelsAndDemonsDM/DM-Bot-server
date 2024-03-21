a9aa1ccd1da42cb55c43b3df1b2b805a178485e3
unknown <neko-d-05@mail.ru>
2024-03-21 22:48:20 +0300

    Разделение main и модуля тестов

--- Code.DM-Bot/etc/auto_docs.py
+++ Code.DM-Bot/etc/auto_docs.py
@@ -1,6 +1,7 @@
 import os
 import re
 
+
 def extract_docstrings(file_content):
     """
     Извлекает документационные строки из содержимого файла.
--- Code.DM-Bot/main.py
+++ Code.DM-Bot/main.py
@@ -1,62 +1,10 @@
 import asyncio
 import os
-import shutil
 
 from etc.auto_docs import generate_documentation
 from etc.logger import LoggerManager
-from tests.test_file_work import test_FileWork
-from tests.test_organ import *
-from tests.test_tag_system import *
+from tests.run_tests import run_tests
 
-
-def del_test_folder(logger):
-    test_folder_path = os.path.join(os.getcwd(), 'Data.DM-Bot', 'test')
-    if os.path.exists(test_folder_path):
-        shutil.rmtree(test_folder_path)
-        logger.debug("Test folder deleted successfully.")    
-
-async def run_tests(logger):
-    """
-    Запускает тесты из папки tests.
-
-    Перед и после запуска тестов проверяет, существует ли папка 'test' в папке 'data'.
-    Если папка существует, она удаляется.
-
-    Returns:
-        bool: True, если все тесты успешно пройдены, в противном случае False.
-    """
-    del_test_folder(logger)
-    
-    all_tests_passed = True
-    
-    logger.debug("===START TESTS===")
-    
-    logger.debug("="*40)
-    logger.debug("Start test_FileWork")
-    all_tests_passed &= await test_FileWork(logger)
-    
-    logger.debug("="*40)
-    logger.debug("Start test_TagData")
-    all_tests_passed &= await test_Tag(logger)
-    
-    logger.debug("="*40)
-    logger.debug("Start test_TagsManager")
-    all_tests_passed &= await test_TagsManager(logger)
-    
-    logger.debug("="*40)
-    logger.debug("Start test_Organ")
-    all_tests_passed &= await test_Organ(logger)
-
-    logger.debug("="*40)
-    logger.debug("Start test_OrganPrototype")
-    all_tests_passed &= await test_OrganPrototype(logger)
-
-    logger.debug("="*40)
-    del_test_folder(logger)
-    
-    logger.debug("===END TESTS===")
-    
-    return all_tests_passed
 
 def show_menu():
     while True:
@@ -110,6 +58,6 @@ async def main():
         
         if menu == 0: # Выход из программы
             return
-    
+
 if __name__ == "__main__":
     asyncio.run(main())
