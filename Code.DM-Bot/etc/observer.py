e46de22bd39a56394a4072518857fd1296dd9802
The many faced demon <themanyfaceddemon@noreply.codeberg.org>
2024-03-21 08:56:22 +0000

    FIX ME
    - добавлена документация функций для наблюдателя
    - добавлена возможность создания временных подписчиков

--- Code.DM-Bot/etc/observer.py
+++ Code.DM-Bot/etc/observer.py
@@ -1,14 +1,38 @@
 class Observer:
-    def __init__(self):
-        self.subscribers = []
+        def __init__(self):
+                """
+                        Инициализирует новый экземпляр класса Observer.
+                                """
+                                        self.subscribers = []
 
-    def attach(self, func):
-        self.subscribers.append(func)
+                                            def attach(self, func, num_notifications=float('inf')):
+                                                    """
+                                                            Подписывает функцию на события.
 
-    def detach(self, func):
-        if func in self.subscribers:
-            self.subscribers.remove(func)
+                                                                    :param func: Функция-подписчик.
+                                                                            :param num_notifications: Количество уведомлений, которые должен получить подписчик (по умолчанию бесконечное количество).
+                                                                                    """
+                                                                                            self.subscribers.append({'func': func, 'num_notifications': num_notifications, 'notifications_received': 0})
 
-    def notify(self, *args, **kwargs):
-        for subscriber in self.subscribers:
-            subscriber(*args, **kwargs)
+                                                                                                def detach(self, func):
+                                                                                                        """
+                                                                                                                Отписывает функцию от событий.
+
+                                                                                                                        :param func: Функция-подписчик.
+                                                                                                                                """
+                                                                                                                                        for subscriber in self.subscribers:
+                                                                                                                                                    if subscriber['func'] == func:
+                                                                                                                                                                    self.subscribers.remove(subscriber)
+
+                                                                                                                                                                        def notify(self, *args, **kwargs):
+                                                                                                                                                                                """
+                                                                                                                                                                                        Оповещает всех подписчиков о событии.
+
+                                                                                                                                                                                                :param args: Позиционные аргументы для передачи подписчикам.
+                                                                                                                                                                                                        :param kwargs: Именованные аргументы для передачи подписчикам.
+                                                                                                                                                                                                                """
+                                                                                                                                                                                                                        for subscriber in self.subscribers[:]:  # Создаем копию списка, чтобы избежать проблем с удалением элементов во время итерации
+                                                                                                                                                                                                                                    subscriber['func'](*args, **kwargs)
+                                                                                                                                                                                                                                                subscriber['notifications_received'] += 1
+                                                                                                                                                                                                                                                            if subscriber['notifications_received'] >= subscriber['num_notifications']:  # Если подписчик получил все уведомления, отписываем его
+                                                                                                                                                                                                                                                                            self.detach(subscriber['func'])
