de086beb8ea27017f62bf91423132d87c8219176
unknown <neko-d-05@mail.ru>
2024-03-24 10:52:34 +0300

    чистка кода

--- Code.DM-Bot/etc/containers.py
+++ Code.DM-Bot/etc/containers.py
@@ -1,18 +1,16 @@
 
 class Item:
     def __init__(self, size: int, item_obj):
-        self._size = size              # Определяем размер предмета в числовом выражении, что ранее занесено   
-        self._item_obj = item_obj        # Определяем к чему принадлежит предмет
+        self._size = size # Определяем размер предмета в числовом выражении, что ранее занесено   
+        self._item_obj = item_obj # Определяем к чему принадлежит предмет
         
     #Get методы
     @property
     def size(self) -> int:
-    
         return self._size
         
     @property    
     def item_obj(self):
-        
         return self._item_obj
         
     #Set методы
@@ -23,27 +21,25 @@ class Item:
     @item_obj.setter
     def item_obj(self, item_obj):
         self._item_obj = item_obj
-        
-    
 
     # Данные о предмете
     @property
     def item_desc(self):
         try:
             return self._item_obj.description
-        except Exception as err:
+        except Exception:
             return None
     
     @property
     def item_name(self):
         try:
             return self._item_obj.name
-        except Exception as err:
+        except Exception:
             return None
         
     @property    
     def item_id(self):
         try:
             return self._item_obj.id
-        except Exception as err:
+        except Exception:
             return None
