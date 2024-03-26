f753c2124063648da9d3d8e4f5ce9fdf2eafee55
The many faced demon <154847721+themanyfaceddemon@users.noreply.github.com>
2024-03-26 13:11:44 +0300

    Завершение работы над прототипом реагентов. Осталось сами реагенты написать

--- Code.DM-Bot/prototype_system/reagent.py
+++ Code.DM-Bot/prototype_system/reagent.py
@@ -1,22 +1,57 @@
+import copy
 from .prototype_loader import PrototypeLoader
 
 
 class ReagentPrototypeLoader(PrototypeLoader):
-    def __init__(self, file_path, effect_list):
+    def __init__(self, file_path, effect_proto_list):
         super().__init__(file_path, "Reagent")
+        self._effect_proto_list = effect_proto_list
     
     def _get_func(self, config):
         return self._create_reagent
     
     def _create_reagent(self, config):
         reagent_info = []
+        solid_effect = []
+        liquid_effect = []
+        gas_effect = []
 
         id = self._validate_config_param(config, "id")
 
         reagent_info.append(self._validate_config_param(config, "name", id))
         reagent_info.append(self._validate_config_param(config, "desc", id))
         available_states = self._validate_config_param(config, "available_states", id)
-
+        
         for state in available_states:
-            if state not in ["solid", "liquid", "gas"]:
-                pass # Похуй, потом. Мозг вмер
+            if state == "solid":
+                solid_effect = self._get_effect_list(config, state, id)
+            elif state == "liquid":
+                liquid_effect = self._get_effect_list(config, state, id)
+            elif state == "gas":
+                gas_effect = self._get_effect_list(config, state, id)
+            else:
+                raise ValueError(f"'{state}' not invluded in the list of avaliable states. reagent:{id}")
+        
+        reagent_info.append(solid_effect)
+
+        reagent_info.append(liquid_effect)
+
+        reagent_info.append(gas_effect)
+
+        return Reagent(*reagent_info)
+
+    def _get_effect_list(self, config, state: str, id: str):
+        state = self._validate_config_param(config, state, id)
+        effect_list = []
+        
+        if state:
+            for effect in state:
+                effect_id = self._validate_config_param(config, "id", id)
+                if effect_id is None:
+                    raise ValueError(f"In reagent:{id} effect id is not found.")
+                effect = copy.deepcopy(self._effect_proto_list[effect_id])
+                tick = self._validate_config_param(config, "tick", id)
+                effect.tick = tick
+                effect_list.append(effect)
+        
+        return effect_list
