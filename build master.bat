c47a22e8d00f64f21a0bee1d783bb188387819f1
The many faced demon <154847721+themanyfaceddemon@users.noreply.github.com>
2024-03-30 15:49:30 +0300

    Добавление сборки проекта

---
+++ build master.bat
@@ -0,0 +1,12 @@
+@echo off
+set "PDIR=%~dp0"
+
+cd /d "%PDIR%"
+
+title DM-Bot build master
+
+python -m nuitka --remove-output --jobs=4 --standalone --onefile --no-pyi-file --windows-icon-from-ico=icon.png Code.DM-Bot/main.py
+
+ren main.exe DM-Bot.exe
+
+pause
