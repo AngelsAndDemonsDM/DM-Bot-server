308194340a025ae40f4dbf42863d96a119073748
The many faced demon <154847721+themanyfaceddemon@users.noreply.github.com>
2024-03-30 19:04:21 +0300

    Update main.py

--- Code.DM-Bot/main.py
+++ Code.DM-Bot/main.py
@@ -8,14 +8,22 @@ from modules.organs_main import organs_main
 from tests.run_tests import run_tests
 
 
+
+def clear_consol():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def pause_consol():
+    input("Нажмите Enter для продолжения...")
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description='DM-Bot')
     parser.add_argument('--debug', action='store_true', help='Включить режим отладки')
     return parser.parse_args()
 
-def show_menu():
+def show_menu_debug():
     while True:
         clear_consol()
+        print("DEBUG MODE!\n")
         print("Меню выбора:")
         print("1. Запуск тестов")
         print("2. Создать документацию")
@@ -28,18 +36,64 @@ def show_menu():
             print("Неверное число. Просьба повторить ввод.")
             pause_consol()
 
-def clear_consol():
-    os.system('cls' if os.name == 'nt' else 'clear')
+async def main_debug():
+    # Объявление менеджеров
+    logger = LoggerManager(True)
 
-def pause_consol():
-    input("Нажмите Enter для продолжения...")
+    # Меню выбора
+    while True:
+        menu = show_menu_debug()
+        
+        if menu == 1: # Запуск тестов
+            anser = await run_tests(logger)
+            if anser:
+                logger.info("Все тесты пройдены удачно")
+            else:
+                logger.error("Один из тестов был провален!")
+            pause_consol()
+            continue
+            
+        if menu == 2: # Генерация документации
+            generate_documentation(logger)
+            pause_consol()
+            continue
+
+        if menu == 3: # Тест системы органов
+            organs_main()
+            continue
+
+        if menu == 0: # Выход из программы
+            return
+
+def print_table(version, created_by):
+    max_created_by_length = max(len(item) for item in created_by)
+    version_length = len(version)
+    top_bottom_line_width = max(version_length, max_created_by_length) + 15
+    print("*" + "-" * (top_bottom_line_width) + "*")
+    print("| Version -", version, " " * (top_bottom_line_width - version_length - 13), "|")
+    print("*" + "-" * (top_bottom_line_width) + "*")
+    for creator in created_by:
+        print("| Created by:", creator, " " * (top_bottom_line_width - len(creator) - 15), "|")
+    print("*" + "-" * (top_bottom_line_width) + "*\n")
+
+def show_menu():
+    while True:
+        clear_consol()
+        print_table("███", ["Многоликий демон - код", "Verggrey - Помощь с кодом и оформление"])
+        print("А ничего не готово для резил билда.\n")
+        print("Меню выбора:")
+        print("0. Выход")
+        choice = input("Введите число: ")
+        if choice in {"0"}:
+            return int(choice)
+        else:
+            print("Неверное число. Просьба повторить ввод.")
+            pause_consol()
+
 
 async def main():
-    args = parse_arguments()
-    debug = args.debug
-
     # Объявление менеджеров
-    logger = LoggerManager(debug)
+    logger = LoggerManager(False)
 
     # Меню выбора
     while True:
@@ -66,5 +120,11 @@ async def main():
         if menu == 0: # Выход из программы
             return
 
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    args = parse_arguments()
+    debug = args.debug
+    if debug:  
+        asyncio.run(main_debug())
+    else:
+        asyncio.run(main())
