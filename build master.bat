8a1f10b23b89e2bb93e8c1f2e18e59f3d18fd36a
The many faced demon <154847721+themanyfaceddemon@users.noreply.github.com>
2024-04-01 11:01:22 +0300

    Update build master.bat

--- build master.bat
+++ build master.bat
@@ -5,6 +5,8 @@ cd /d "%PDIR%"
 
 title DM-Bot build master
 
+color 6
+
 del DM-Bot.exe /q
 
 python -m nuitka --remove-output --jobs=4 --standalone --onefile --no-pyi-file --windows-icon-from-ico=Sprites.DM-Bot/exe-icon.png Code.DM-Bot/main.py
