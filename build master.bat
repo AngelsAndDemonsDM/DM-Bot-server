04afb3985e327481ff0467c93d393013eb781991
The many faced demon <154847721+themanyfaceddemon@users.noreply.github.com>
2024-04-01 11:43:00 +0300

    update *.bat

--- build master.bat
+++ build master.bat
@@ -1,16 +1,11 @@
 @echo off
 set "PDIR=%~dp0"
-
 cd /d "%PDIR%"
 
 title DM-Bot build master
-
-color 6
-
 del DM-Bot.exe /q
 
 python -m nuitka --remove-output --jobs=4 --standalone --onefile --no-pyi-file --windows-icon-from-ico=Sprites.DM-Bot/exe-icon.png Code.DM-Bot/main.py
-
 ren main.exe DM-Bot.exe
 
 exit
--- start-debug.bat
+++ start-debug.bat
@@ -1,16 +1,12 @@
 @echo off
 set "PDIR=%~dp0"
-
 cd /d "%PDIR%"
 
 title DM-Bot
-
 python Code.DM-Bot/main.py --debug
 
 for /d /r %%d in (__pycache__) do (
   rmdir /s /q "%%d" 2>nul
 )
-
 pause
-
 exit
