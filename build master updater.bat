e2174a06d7787e645b975980d78adbaf829eb60b
The many faced demon <154847721+themanyfaceddemon@users.noreply.github.com>
2024-04-04 14:36:54 +0300

    fix bug

--- build master updater.bat
+++ build master updater.bat
@@ -5,7 +5,7 @@ cd /d "%PDIR%"
 title DM-Bot build master updater
 del DM-Bot-updater.exe /q
 
-python -m nuitka --remove-output --jobs=4 --standalone --onefile --no-pyi-file --windows-icon-from-ico=Sprites.DM-Bot/exe-updater-icon.png Updater.DM-Bot/main.py
-ren main.exe DM-Bot-updater.exe
+python -m nuitka --remove-output --jobs=4 --standalone --onefile --no-pyi-file --windows-icon-from-ico=Sprites.DM-Bot/exe-updater-icon.png Updater.DM-Bot/updater_main.py
+ren updater_main.exe DM-Bot-updater.exe
 
 exit
