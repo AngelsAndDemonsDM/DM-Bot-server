6d7e1b52ced7be76429ca4a93a6ed93be9b4ab63
unknown <neko-d-05@mail.ru>
2024-04-06 03:20:00 +0300

    add remake dir

--- Code.DM-Bot/remake/mind.py
+++ Code.DM-Bot/remake/mind.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union # DELETE IT!
 
 from .body import Body
 
--- Code.DM-Bot/remake/race.py
+++ Code.DM-Bot/remake/race.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union # DELETE IT!
 
 from .effect import EffectsList
 
