0aadce490b8ea8f520e2c01c6eec9d6d1b3d4fd4
unknown <neko-d-05@mail.ru>
2024-04-08 10:26:17 +0300

    Ересь с графикой

--- Add in future/graphic.py
+++ Add in future/graphic.py
@@ -2,22 +2,35 @@ import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 
-def main():
+# Забей хуй и используй pyqt5
+
+def main_window():
+    # Main
     pygame.init()
-    pygame.display.set_mode((400, 300))
+    screen = pygame.display.set_mode((500, 500))
     pygame.display.set_caption("DM-Bot")
     pygame.display.set_icon(pygame.image.load("Sprites.DM-Bot\exe-main-icon.png"))
+
+    # Fronts
+    base_font = pygame.font.Font("Data.DM-Bot\\Fonts\\base_font.otf", 40)
+    text_surface = base_font.render("????", False, "black")
+
+    # Main loop
     is_run = True
     clock = pygame.time.Clock()
     FPS_limit = 60
 
     while is_run:
+        screen.blit(text_surface, (0, 0))
+
+        clock.tick(FPS_limit)
+        pygame.display.update()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_run = False
-            
-        pygame.display.flip()
-        clock.tick(FPS_limit)
+    
+    pygame.quit()
 
 if __name__ == "__main__":
-    main()
+    main_window()
