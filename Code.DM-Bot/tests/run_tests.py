362bc95da2b8e0e70faf7aaab7d868de1e06721d
The many faced demon <neko-d-05@mail.ru>
2024-04-16 13:14:25 +0300

    Update run_tests.py

--- Code.DM-Bot/tests/run_tests.py
+++ Code.DM-Bot/tests/run_tests.py
@@ -7,7 +7,7 @@ from .test_observer import TestObserver
 from .test_tag import TestTagsManager
 
 
-def del_test_folder(): 
+def del_test_folder() -> None: 
     test_folder_path = os.path.join(os.getcwd(), 'Data.DM-Bot', 'test')
     if os.path.exists(test_folder_path):
         shutil.rmtree(test_folder_path)
@@ -20,8 +20,10 @@ def run_tests() -> None:
     Если папка существует, она удаляется.
     """
     del_test_folder()
+
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
+
     suite.addTests(loader.loadTestsFromTestCase(TestObserver))
     suite.addTests(loader.loadTestsFromTestCase(TestFileWork))
     suite.addTests(loader.loadTestsFromTestCase(TestTagsManager))
