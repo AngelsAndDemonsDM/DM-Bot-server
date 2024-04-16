ba204662cbbc3a6977b6a75ab0e7269403d60d43
The many faced demon <neko-d-05@mail.ru>
2024-04-16 10:45:44 +0300

    update reagent docs

--- Code.DM-Bot/reagent/reagent.py
+++ Code.DM-Bot/reagent/reagent.py
@@ -4,6 +4,16 @@ from etc.base_classes import BaseObject
 
 class Reagent(BaseObject):
     def __init__(self, id: str, name: str, description: str, boiling_temp: float, crystal_temp: float, temp: float, action: list[BaseReagentState] = None) -> None:
+        """
+        Инициализация объекта Reagent.
+        Наследуется от BaseObject.
+
+        Attributes:
+            boiling_temp (float): Температура кипения.
+            crystal_temp (float): Температура кристализации.
+            temp (float): Текущая температура
+            action (list[BaseReagentState], optional): Бля. По умолчанию None. (Я не помню)
+        """
         super().__init__(id, name, description)
         
         self._boiling_temp = boiling_temp
