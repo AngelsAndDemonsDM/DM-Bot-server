67043ce93ffe0c2c8341ee1c35aee451efe56899
The many faced demon <neko-d-05@mail.ru>
2024-04-16 12:34:57 +0300

    add type for effect

--- Code.DM-Bot/effect.py
+++ Code.DM-Bot/effect.py
@@ -2,21 +2,27 @@ from etc.base_classes.base_object import BaseObject
 
 
 class Effect(BaseObject):
-    def __init__(self, id: str, name: str, description: str, strength: int, tick: int = -1):
+    def __init__(self, id: str, name: str, description: str, type: str, strength: float, tick: int = -1):
         """
         Инициализация объекта эффекта.
 
         Args:
+            type (str): Тип эффекта
             power (float): Сила эффекта.
             tick (int, optional): Количество тиков до завершения эффекта. По умолчанию -1.
         """
         super.__init__(id, name, description)
-        self._strength = strength
-        self._tick = tick
+        self._type: str = type
+        self._strength: float = strength
+        self._tick: int = tick
     
     # Get metods
     @property
-    def strength(self) -> str:
+    def type(self) -> str:
+        return self._type
+
+    @property
+    def strength(self) -> float:
         return self._strength
     
     @property
@@ -25,7 +31,7 @@ class Effect(BaseObject):
 
     # Set metods
     @tick.setter
-    def tick(self, value: int):
+    def tick(self, value: int) -> None:
         if value <= -1:
             self._tick = -1
         
