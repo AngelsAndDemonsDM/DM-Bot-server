50a34afe2a45173218df0ad5415e66967de0ef40
The many faced demon <neko-d-05@mail.ru>
2024-04-17 00:25:29 +0300

    version 0.0.01

--- build master main.bat
+++ build master main.bat
@@ -3,11 +3,11 @@ set "PDIR=%~dp0"
 cd /d "%PDIR%"
 
 title DM-Bot build master main
-del DM-Bot.exe /q
 
-python -m nuitka --remove-output --jobs=4 --standalone --onefile --no-pyi-file --windows-icon-from-ico=Sprites.DM-Bot/exe-main-icon.png Code.DM-Bot/main.py
+del DM-Bot.exe /q
+python -m nuitka --remove-output --jobs=4 --standalone --onefile --no-pyi-file --windows-icon-from-ico=Sprites.DM-Bot/icons/exe-main-icon.png Code.DM-Bot/main.py
 ren main.exe DM-Bot.exe
 
-python create_zip.py --name=DM-Bot.exe
+python create_zip.py --file1=DM-Bot.exe --output=DM-Bot
 
 exit
--- create_zip.py
+++ create_zip.py
@@ -6,13 +6,24 @@ import pyminizip
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='DM-Bot')
-    parser.add_argument('--name', help='Name of the file to be added to the protected archive')
+    parser.add_argument('--file1', help='Name of the first file to be added to the protected archive')
+    parser.add_argument('--file2', help='Name of the second file to be added to the protected archive')
+    parser.add_argument('--output', help='Name of the output archive')
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_arguments()
-    name = args.name
-    if name:
-        pyminizip.compress(name, None, name[:-4] + ".zip", b"1Ei2ttDIBadNmDHqh3HRIWpipnxh7DwNM", 0) 
+    file1 = args.file1
+    file2 = args.file2
+    output_zip_name = args.output + ".zip" if args.output else "archive.zip"
+
+    if file1 or file2:
+        # Пароль для архива
+        password = b"1Ei2ttDIBadNmDHqh3HRIWpipnxh7DwNM"
+        
+        # Упаковка двух файлов в архив
+        pyminizip.compress_multiple([file1, file2], [None, None], output_zip_name, password, 0)
+        
+        print(f"File(s) are compressed into {output_zip_name}")
     else:
-        print("Specify filename using argument --name")
+        print("Specify filenames using arguments --file1 and --file2")
