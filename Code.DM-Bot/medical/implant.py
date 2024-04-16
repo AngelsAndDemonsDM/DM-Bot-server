90faf88c4930d772b473e74cbea6ed551a565b97
The many faced demon <neko-d-05@mail.ru>
2024-04-16 12:36:59 +0300

    Create implant.py

---
+++ Code.DM-Bot/medical/implant.py
@@ -0,0 +1,5 @@
+from etc.base_classes.base_object import BaseObject
+
+class Implant(BaseObject):
+    def __init__(self, id: str, name: str, description: str) -> None:
+        super().__init__(id, name, description)
