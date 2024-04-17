0981827c45bca3e128706c51d7cf8d4e028f2baf
The many faced demon <neko-d-05@mail.ru>
2024-04-17 16:20:48 +0300

    update build

--- pack.py
+++ pack.py
@@ -1,29 +1,74 @@
-import argparse
+import logging
 import os
+import shutil
+import sys
 
-import pyminizip
+import pyzipper
 
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='DM-Bot')
-    parser.add_argument('--file1', help='Name of the first file to be added to the protected archive')
-    parser.add_argument('--file2', help='Name of the second file to be added to the protected archive')
-    parser.add_argument('--output', help='Name of the output archive')
-    return parser.parse_args()
+def copy_folders(destination, folders):
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    source_path = os.path.join(current_directory, "Code.DM-Bot")
+
+    for folder in folders:
+        source_folder = os.path.join(source_path, folder)
+        destination_folder = os.path.join(destination, folder)
+        
+        if os.path.exists(destination_folder):
+            shutil.rmtree(destination_folder)
+        
+        shutil.copytree(source_folder, destination_folder)
+
+def zip_folder(folder_path, output_path, password, compression=pyzipper.ZIP_DEFLATED, encryption=pyzipper.WZ_AES):
+    parent_folder = os.path.dirname(folder_path)
+    contents = os.walk(folder_path)
+    
+    # Инициализация логгера
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    
+    try:
+        with pyzipper.AESZipFile(output_path, 'w', compression=compression, encryption=encryption) as zip_file:
+            zip_file.setpassword(password)
+            
+            for root, folders, files in contents:
+                for folder_name in folders:
+                    absolute_path = os.path.join(root, folder_name)
+                    relative_path = os.path.relpath(absolute_path, parent_folder)
+                    logging.info(f"Adding '{absolute_path}' to archive.")
+                    zip_file.write(absolute_path, relative_path)
+
+                for file_name in files:
+                    absolute_path = os.path.join(root, file_name)
+                    relative_path = os.path.relpath(absolute_path, parent_folder)
+                    logging.info(f"Adding '{absolute_path}' to archive.")
+                    zip_file.write(absolute_path, relative_path)
+
+        logging.info(f"'{output_path}' created successfully.")
+
+    except FileNotFoundError as e:
+        logging.error(f"File or directory not found: {e}")
+        sys.exit(1)
+    except pyzipper.PyZipperError as e:
+        logging.error(f"PyZipper error: {e}")
+        sys.exit(1)
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
-    args = parse_arguments()
-    file1 = args.file1
-    file2 = args.file2
-    output_zip_name = args.output + ".zip" if args.output else "archive.zip"
-
-    if file1 or file2:
-        # Пароль для архива
-        password = b"1Ei2ttDIBadNmDHqh3HRIWpipnxh7DwNM"
-        
-        # Упаковка двух файлов в архив
-        pyminizip.compress_multiple([file1, file2], [None, None], output_zip_name, password, 0)
-        
-        print(f"File(s) are compressed into {output_zip_name}")
-    else:
-        print("Specify filenames using arguments --file1 and --file2")
+    destination_folder = "DM-Bot"
+    output_zip_name = destination_folder + ".zip"
+    password = b"1Ei2ttDIBadNmDHqh3HRIWpipnxh7DwNM"
+    
+    folder1 = "templates"
+    folder2 = "static"
+    
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+    
+    copy_folders(destination_folder, [folder1, folder2])
+    
+    zip_folder(destination_folder, output_zip_name, password)
+    #pyminizip.compress(destination_folder, None, output_zip_name, password, 0)
+    
+    print(f"Selected folders are copied to {destination_folder} and compressed into {output_zip_name} with encryption")
