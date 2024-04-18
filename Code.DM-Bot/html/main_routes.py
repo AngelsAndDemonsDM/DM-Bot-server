237f00961a94c8ad0cc945841e51719a029f24be
unknown <neko-d-05@mail.ru>
2024-04-18 21:06:01 +0300

    add comments

--- Code.DM-Bot/html/main_routes.py
+++ Code.DM-Bot/html/main_routes.py
@@ -6,14 +6,17 @@ from .pages.index import index
 
 main_bp = Blueprint('main', __name__)
 
+# Главное окно приложения
 @main_bp.route('/')
 def home_page():
     return index()
 
+# Окно с управлением ботом 
 @main_bp.route("/bot")
 def bot_page():
     return bot_main_page()
 
+# О говнюках =)
 @main_bp.route('/about')
 def about_page():
     return about()
