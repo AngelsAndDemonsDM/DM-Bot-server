c7b7419082cae86ce9c61d9dda2579c570635dd6
The many faced demon <neko-d-05@mail.ru>
2024-04-19 17:45:32 +0300

    Update main_bot_core.py

--- Code.DM-Bot/bot/main_bot_core.py
+++ Code.DM-Bot/bot/main_bot_core.py
@@ -4,10 +4,11 @@ from discord.ext import commands
 
 from . import bot
 
+TOKEN_PATH: str = "secrets/token.bin"
 
 @bot.command()
 async def ping(ctx: discord.Interaction):
     await ctx.send(f"Pong in ({round(bot.latency * 1000)}ms)")
 
 async def main():
-    await bot.start(FileWork("secrets/token.bin").load_data())
+    await bot.start(FileWork(TOKEN_PATH).load_data())
