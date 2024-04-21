7045d456787d915ebb91e70d1fe6add9af74bf6f
themanyfaceddemon <neko-d-05@mail.ru>
2024-04-21 06:51:10 +0300

    add if if players is None

--- Code.DM-Bot/html/pages/players.py
+++ Code.DM-Bot/html/pages/players.py
@@ -6,18 +6,23 @@ from player.soul import PlayerSoul
 def players_main_page():
     return render_template('player.html')
 
-
 @socketio.on('getAllPlayers')
 def handle_get_all_players():
     player_list = []
-    players:list [PlayerSoul] = FileWork("Discord/players")
-    
-    for player in players:
-        player_dict = {}
-
-        player_dict["id"] = player.id
-        player_dict["name"] = player.name
-
-        player_list.append(player_dict)
-    
+    try:
+        players: list[PlayerSoul] = FileWork("Discord/players")
+        
+        for player in players:
+            player_dict = {}
+            player_dict["id"] = player.id
+            player_dict["name"] = player.name
+            player_list.append(player_dict)
+        
+        # Если player_list пустой, добавляем плейсхолдер игрока
+    except Exception:
+        player_list.append({
+            "id": "placeholder",
+            "name": "Нет доступных игроков"
+        })
+        
     socketio.emit('allPlayers', player_list)
