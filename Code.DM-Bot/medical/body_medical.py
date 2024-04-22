ba4d0c4a28ea8c7e7bcea8d7b71d12a9d09cb99f
themanyfaceddemon <neko-d-05@mail.ru>
2024-04-22 21:27:07 +0300

    Update body_medical.py

--- Code.DM-Bot/medical/body_medical.py
+++ Code.DM-Bot/medical/body_medical.py
@@ -1,52 +1,75 @@
+from .limb import Limb
+from etc import Effect
+
 class BodyMedical:
-    def __init__(self) -> None:   
+    def __init__(self) -> None:
         # Stats
-        self._body_stats = {
-            "cognitive": self.cognitive,       # сознание
-            "pain": self.pain,                 # боль
-            "vision": self.vision,             # зрение
-            "hearing": self.hearing,           # слух
-            "scent": self.scent,               # обоняние
-            "productivity": self.productivity, # работа
-            "movement": self.movement,         # передвижение
-            "fertility": self.fertility,       # фертильность
+        self._body_stats: dict = { # Инициализируем всё по 0.0. Мне похуй.
+            "cognitive": 0.0,    # сознание
+            "pain": 0.0,         # боль
+            "vision": 0.0,       # зрение
+            "hearing": 0.0,      # слух
+            "scent": 0.0,        # обоняние
+            "productivity": 0.0, # работа
+            "movement": 0.0,     # передвижение
+            "fertility": 0.0,    # фертильность
         }
 
         # Needs
         # TODO
 
         # Limbs
+        self._limbs: list[Limb]
         # TODO
-        
-    
-    @property
-    def cognitive(self):
-        ...
-    
-    @property
-    def pain(self):
-        ...
 
-    @property
-    def vision(self):
-        ...
+    def get_stat(self, key: str):
+        """
+        Возвращает значение статистики по ключу.
 
-    @property
-    def hearing(self):
-        ...
+        Значение ключей:
+        cognitive:    сознание
+        pain:         боль
+        vision:       зрение
+        hearing:      слух
+        scent:        обоняние
+        productivity: работа
+        movement:     передвижение
+        fertility:    фертильность
 
-    @property
-    def scent(self):
-        ...
+        Args:
+            key (str): Ключ статистики.
 
-    @property
-    def productivity(self):
-        ...
+        Returns:
+            value(Any or None): Возвращает или значение статистики или None если не нашёл ключ.
+        """
+        return self._body_stats.get(key, None)
 
-    @property
-    def movement(self):
-        ...
+    def update_stats(self) -> None:
 
-    @property
-    def fertility(self):
-        ...
+        self._body_stats: dict = { # Сносим значения до 0. Потом считаем
+            "cognitive": 0.0,    # сознание
+            "pain": 0.0,         # боль
+            "vision": 0.0,       # зрение
+            "hearing": 0.0,      # слух
+            "scent": 0.0,        # обоняние
+            "productivity": 0.0, # работа
+            "movement": 0.0,     # передвижение
+            "fertility": 0.0,    # фертильность
+        }
+
+        # О боже блять | TODO ВЫЧИСЛЕНИЯ ТУТ ПРОСТО КОНСКИЕ БЛЯТЬ. УПРОСТИТЬ
+        # Создание списка всех эффектов
+        diseases_effects: list[Effect] = [effect for limb in self._limbs for effect in limb.diseases.effects]
+        implants_effects: list[Effect] = [effect for limb in self._limbs for effect in limb.implants.effects]
+        organs_effects: list[Effect]   = [effect for limb in self._limbs for effect in limb.organs.effects]
+
+        # Объединение всех эффектов в один список
+        all_effects = diseases_effects + implants_effects + organs_effects
+
+        for effect in all_effects:
+            if effect.type.startswith("body_") and effect.type.endswith("_mod"):
+                stat_name = effect.type.split("_")[1]
+                        
+                if stat_name in self._body_stats:
+                    self._body_stats[stat_name] += effect.strength
+        # brain dead
