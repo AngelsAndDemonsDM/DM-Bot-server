35b6ec3a31a2ec14bd66073891dfd779d9f4b0a0
themanyfaceddemon <neko-d-05@mail.ru>
2024-05-18 21:06:42 +0300

    Обновление команд под новое бд

--- Code.DM-Bot/db_work/__init__.py
+++ Code.DM-Bot/db_work/__init__.py
@@ -1,3 +1,3 @@
-from db_work.AsyncSQLDB import AsyncSQLDB
+from db_work.AsyncDB import (DBF_AUTOINCREMENT, DBF_NOT_NULL, DBF_PRIMARY_KEY,
+                             DBF_UNIQUE, AsyncDB)
 from db_work.bin_file_data import BinFileData
-from db_work.SQLDB import SQLDB
