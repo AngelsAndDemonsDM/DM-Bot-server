771ecbf15ea48ec766f50fcb6e55873fa854f17f
themanyfaceddemon <neko-d-05@mail.ru>
2024-05-28 02:02:45 +0300

    Фикс импортов

--- Code/base_classes/__init__.py
+++ Code/base_classes/__init__.py
@@ -1,4 +1,4 @@
-from base_classes.base_object import BaseObject
-from base_classes.base_entity import Entity
 from base_classes.base_component import Component
+from base_classes.base_entity import Entity
+from base_classes.base_object import BaseObject
 from base_classes.dynamic_loader import DynamicLoader
