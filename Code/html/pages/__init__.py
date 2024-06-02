42aa9962f80ef0e5ba70384a13e969a725257d8d
themanyfaceddemon <neko-d-05@mail.ru>
2024-06-02 15:18:26 +0300

    Update __init__.py

--- Code/html/pages/__init__.py
+++ Code/html/pages/__init__.py
@@ -1,7 +1,6 @@
 
-from html.pages.settings import settings_main_page
-
 from html.pages.about import about_main_page
 from html.pages.bot import bot_main_page
 from html.pages.index import index_main_page
 from html.pages.players import players_main_page
+from html.pages.settings import settings_main_page
