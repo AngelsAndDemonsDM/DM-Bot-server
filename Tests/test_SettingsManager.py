0caefed8564fc1c2a0547d82230cdd2c9550a1ea
themanyfaceddemon <neko-d-05@mail.ru>
2024-06-05 01:03:25 +0300

    Update test_SettingsManager.py

--- Tests/test_SettingsManager.py
+++ Tests/test_SettingsManager.py
@@ -7,10 +7,6 @@ from Code.db_work.SettingsManager import SettingsManager
 class TestSettingsManager(unittest.IsolatedAsyncioTestCase):
     async def asyncSetUp(self):
         self.settings_manager = SettingsManager()
-        
-        if os.path.exists(self.settings_manager._path):
-            os.remove(self.settings_manager._path)
-        
         await self.settings_manager.start()
 
     async def test_create_file(self):
