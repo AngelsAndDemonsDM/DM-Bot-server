6cc527086399b190e1e8d2978019832288ac40c8
themanyfaceddemon <neko-d-05@mail.ru>
2024-06-14 18:13:38 +0300

    Блять

--- Code/bg_task/__init__.py
+++ Code/bg_task/__init__.py
@@ -5,5 +5,5 @@ from Code.db_work import SettingsManager
 async def main_bg_task():
     settings_manager = SettingsManager()
     
-    if settings_manager.get_setting("bot.auto_start"):
+    if await settings_manager.get_setting("bot.auto_start"):
         await bot_start()
