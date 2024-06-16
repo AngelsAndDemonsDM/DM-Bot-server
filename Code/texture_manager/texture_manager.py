46766c119b948e32a60d4dbc6f0f8de9bb3c1a35
themanyfaceddemon <neko-d-05@mail.ru>
2024-06-17 01:22:32 +0300

    Переезд и рефакторинг колор ворка

---
+++ Code/texture_manager/texture_manager.py
@@ -0,0 +1,35 @@
+from typing import Union
+
+from PIL import Image
+
+from Code.texture_manager import HEXColor, RGBColor
+
+
+class Texture:
+    def __init__(self) -> None:
+        pass
+
+    def recolor_mask(self, mask_path: str, color: Union[HEXColor, RGBColor]) -> Image.Image:
+        if isinstance(color, HEXColor):
+            color = color.get_rgba()
+        
+        elif isinstance(color, RGBColor):
+            color = color.cur_value
+        
+        else:
+            raise ValueError("The color passed to recolor_mask must be an instance of HEXColor or RGBColor")
+
+        mask_image = Image.open(mask_path).convert("RGBA")
+       
+        new_data = [
+            (
+                int(pixel[0] * color[0] / 255),
+                int(pixel[0] * color[1] / 255),
+                int(pixel[0] * color[2] / 255),
+                pixel[3]
+            ) if pixel[3] != 0 else pixel
+            for pixel in mask_image.getdata()
+        ]
+        mask_image.putdata(new_data)
+        
+        return mask_image
