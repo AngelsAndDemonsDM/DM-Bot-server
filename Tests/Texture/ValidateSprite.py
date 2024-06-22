2e79f29298a6faf04b121b53f504104c7fd71202
themanyfaceddemon <neko-d-05@mail.ru>
2024-06-22 11:58:35 +0300

    Update ValidateSprite.py

--- Tests/Texture/ValidateSprite.py
+++ Tests/Texture/ValidateSprite.py
@@ -9,8 +9,7 @@ class TestSpriteFolders(unittest.TestCase):
         self.dms_validator = DMSValidator('Sprites')
 
     def test_validate_all_dms_folders(self):
-        """Тестирует функцию validate_all_dms на корневой директории.
-        """
+        """Тестирует функцию validate_all_dms на корневой директории."""
         try:
             result = self.dms_validator.validate_all_dms()
             self.assertTrue(result)
