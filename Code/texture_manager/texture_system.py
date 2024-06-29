28a19cb0514d071c7b8676f546204fea6e4ebead
themanyfaceddemon <neko-d-05@mail.ru>
2024-06-29 15:00:30 +0300

    Update texture_system.py

--- Code/texture_manager/texture_system.py
+++ Code/texture_manager/texture_system.py
@@ -340,7 +340,7 @@ class TextureSystem:
 
         for images in base_images:
             for img in images:
-                print(f"Image size: {img.size}")
+                logging.error(f"Image size: {img.size}")
 
         if any(isinstance(images, list) for images in base_images):
             max_frames = max(len(images) for images in base_images)
