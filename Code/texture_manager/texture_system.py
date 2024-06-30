d95afccf7dc9b302d63cee648f7a2c1e659b6307
themanyfaceddemon <neko-d-05@mail.ru>
2024-06-30 16:17:35 +0300

    Перерыв

--- Code/texture_manager/texture_system.py
+++ Code/texture_manager/texture_system.py
@@ -1,36 +1,53 @@
 import os
-from typing import Any, Dict, List, Tuple
+from functools import wraps
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 import yaml
-from PIL import Image
-from texture_manager.texture_validator import DMSValidator
+from PIL import Image, ImageSequence
+
+
+def check_and_get_compiled(func: Callable) -> Callable:
+    """Декоратор для проверки наличия скомпилированного изображения или GIF-файла.
+
+    Args:
+        func (Callable): Функция, к которой применяется декоратор.
+
+    Returns:
+        Callable: Обернутая функция, которая сначала проверяет наличие скомпилированного файла. Если файл найден, он возвращается. Если нет, выполняется оригинальная функция.
+    """
+    @wraps(func)
+    def wrapper(path: str, state: str, *args, **kwargs) -> Any:
+        color = kwargs.get('color', None)
+        is_gif = kwargs.get('is_gif', False)
+        image = TextureSystem._get_compiled(path, state, color, is_gif)
+        if image:
+            return image
+        
+        return func(path, state, *args, **kwargs)
+    
+    return wrapper
 
 
 class TextureSystem:
-    __slots__ = ['_sprite_path']
+    __slots__ = []
+    DEFAULT_FPS: int = 24
+    DEFAULT_COLOR: Tuple[int, int, int, int] = (255, 255, 255, 255)
     
-    def __init__(self, path: str) -> None:
-        """Класс для работы со спрайтами. Используйте уже объявленный из Code.main_impt
-
-        Args:
-            path (str): Путь до папки со всеми спрайтами
-        """
-        DMSValidator(path).validate_all_dms()
-        base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-        self._sprite_path = os.path.join(base_path, path.replace('/', os.sep))
+    def __init__(self) -> None:
+        pass
 
     @staticmethod
     def _slice_image(image: Image.Image, frame_width: int, frame_height: int, num_frames: int) -> List[Image.Image]:
-        """Нарезает изображение на кадры.
+        """Нарезает изображение на кадры заданного размера.
 
         Args:
-            image (Image.Image): Исходное изображение.
-            frame_width (int): Ширина кадра.
-            frame_height (int): Высота кадра.
-            num_frames (int): Количество кадров.
+            image (Image.Image): Исходное изображение, которое нужно нарезать.
+            frame_width (int): Ширина каждого кадра.
+            frame_height (int): Высота каждого кадра.
+            num_frames (int): Общее количество кадров, которые нужно вырезать из изображения.
 
         Returns:
-            List[Image.Image]: Список кадров.
+            List[Image.Image]: Список вырезанных кадров в виде объектов Image.
         """
         frames = []
         image_width, _ = image.size
@@ -46,43 +63,105 @@ class TextureSystem:
         return frames
 
     @staticmethod
-    def get_color_str(color: Tuple[int, int, int, int]) -> str:
-        """Преобразует цвет в строку с разделителями.
+    def _get_color_str(color: Tuple[int, int, int, int]) -> str:
+        """_summary_
 
         Args:
-            color (Tuple[int, int, int, int]): Цвет в формате RGBA.
+            color (Tuple[int, int, int, int]): _description_
 
         Returns:
-            str: Цвет в формате строки.
+            str: _description_
         """
+        TextureSystem._validate_color(color)
         return '_'.join(map(str, color))
     
     @staticmethod
-    def validate_color(color: Tuple[int, int, int, int]) -> None:
-        """Проверка валидности цвета в формате RGBA.
+    def _validate_color(color: Tuple[int, int, int, int]) -> None:
+        """_summary_
 
         Args:
-            color (Tuple[int, int, int, int]): Цвет в формате RGBA.
+            color (Tuple[int, int, int, int]): _description_
 
         Raises:
-            ValueError: Если значения цвета не находятся в диапазоне от 0 до 255.
+            ValueError: _description_
         """
         if not all(0 <= c <= 255 for c in color):
             raise ValueError("Invalid RGBA color format for texture. All values must be between 0 и 255")
 
     @staticmethod
     def get_textures(path: str) -> List[Dict[str, Any]]:
-        """Получение списка состояний текстур из файла info.yml.
+        """_summary_
 
         Args:
-            path (str): Путь до папки с файлом info.yml.
+            path (str): _description_
 
         Returns:
-            List[Dict[str, Any]]: Список словарей с состояниями текстур.
+            List[Dict[str, Any]]: _description_
         """
         with open(f"{path}/info.yml", 'r') as file:
             info = yaml.safe_load(file)
         
         return info.get('Sprites', [])
 
+    @staticmethod
+    def _get_compiled(path: str, state: str, color: Optional[Tuple[int, int, int, int]] = None, is_gif: bool = False) -> Union[Image.Image, List[Image.Image], None]:
+        """Получает скомпилированное изображение или GIF.
+
+        Args:
+            path (str): Путь до папки с изображениями.
+            state (str): Имя состояния изображения.
+            color (Optional[Tuple[int, int, int, int]]): Цвет для перекрашивания. По умолчанию None.
+            is_gif (bool): Флаг, указывающий, что нужно вернуть GIF. По умолчанию False.
+
+        Returns:
+            Union[Image.Image, List[Image.Image], None]: Изображение или список кадров GIF, или None, если файл не найден.
+        """
+        image_path: str = f"{path}/{state}"
+        if color:
+            image_path += f"_compiled_{TextureSystem._get_color_str(color)}"
+        
+        image_path += ".gif" if is_gif else ".png"
+        
+        if os.path.exists(image_path):
+            with Image.open(image_path) as img:
+                if is_gif:
+                    return [frame.copy() for frame in ImageSequence.Iterator(img)]
+                else:
+                    return img.copy()
+        else:
+            return None
     
+    @staticmethod
+    @check_and_get_compiled
+    def get_image_recolor(path: str, state: str, color: Tuple[int, int, int, int] = DEFAULT_COLOR) -> Image.Image:
+        with Image.open(f"{path}/{state}.png") as image:
+            image = image.convert("RGBA")
+            new_colored_image = [
+                (
+                    int(pixel[0] * color[0] / 255),
+                    int(pixel[0] * color[1] / 255),
+                    int(pixel[0] * color[2] / 255),
+                    pixel[3]
+                ) if pixel[3] != 0 else pixel
+                for pixel in image.getdata()
+            ]
+
+            image.putdata(new_colored_image)
+            image.save(f"{path}/{state}_compiled_{TextureSystem._get_color_str(color)}.png")
+            return image
+    
+    @staticmethod
+    @check_and_get_compiled
+    def get_image(path: str, state: str) -> Image.Image:
+        raise FileNotFoundError(f"Image file for state '{state}' not found in path '{path}'.") # lol
+    
+    @staticmethod
+    @check_and_get_compiled
+    def get_gif_recolor(path: str, state: str, fps: int = DEFAULT_FPS, color: Tuple[int, int, int, int] = DEFAULT_COLOR):
+        pass
+
+    
+    @staticmethod
+    @check_and_get_compiled
+    def get_gif(path: str, state: str, fps: int = DEFAULT_FPS):
+        pass
