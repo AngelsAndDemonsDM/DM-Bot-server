1ef5ea3c2fb54bcf29fbf0039015643f1effddf1
themanyfaceddemon <neko-d-05@mail.ru>
2024-07-01 18:46:10 +0300

    Update __init__.py

--- Code/html_code/pages/__init__.py
+++ Code/html_code/pages/__init__.py
@@ -1,5 +1,5 @@
-from py_html.pages.about import render_about_main_page
 from html_code.pages.changelog import render_changelog_main_page
+from py_html.pages.about import render_about_main_page
 from py_html.pages.index import render_index_main_page
 from py_html.pages.players import render_players_main_page
 
