2a49d60158ccdad609ef8e47e8acc6087625a079
themanyfaceddemon <neko-d-05@mail.ru>
2024-07-06 18:14:29 +0300

    fix import

--- Code/access_manager/__init__.py
+++ Code/access_manager/__init__.py
@@ -1,3 +1,2 @@
-from access_manager.access_flags import (CAN_ALLOW_CREATE_USER,
-                                         CAN_CHANGE_PASSWORD, CAN_DELETE_USERS)
+from access_manager.access_flags import CAN_CHANGE_PASSWORD, CAN_DELETE_USERS
 from access_manager.auth_manager import AuthManager
