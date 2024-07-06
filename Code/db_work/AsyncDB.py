3f0fac8831570e7ada976e7da5bf6ac5871b0999
themanyfaceddemon <neko-d-05@mail.ru>
2024-07-06 09:20:04 +0300

    update tests

--- Code/db_work/AsyncDB.py
+++ Code/db_work/AsyncDB.py
@@ -5,6 +5,7 @@ from typing import Any, Dict, List, Optional, Tuple
 
 import aiosqlite
 
+
 class AsyncDBError(Exception):
     """Базовый класс для исключений AsyncDB.
     """
--- Code/db_work/__init__.py
+++ Code/db_work/__init__.py
@@ -1,2 +1,4 @@
-from db_work.AsyncDB import AsyncDB
+from db_work.AsyncDB import (AsyncDB, CheckConstraintError,
+                             ForeignKeyConstraintError, NotNullConstraintError,
+                             UniqueConstraintError)
 from db_work.SettingsManager import SettingsManager
