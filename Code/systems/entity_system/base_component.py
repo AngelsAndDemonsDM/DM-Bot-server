fc42df3c8a0843dc6484ce4f5efa175fc914e54d
themanyfaceddemon <neko-d-05@mail.ru>
2024-07-08 16:11:00 +0300

    Небольшое обновление

--- Code/systems/entity_system/base_component.py
+++ Code/systems/entity_system/base_component.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Any, Dict, Type
 
+
 class BaseComponent(ABC):
     __slots__ = ['comp_type', 'owner']
     
--- Code/systems/entity_system/factory.py
+++ Code/systems/entity_system/factory.py
@@ -1,5 +1,5 @@
 import os
-from typing import Dict, List, Type
+from typing import Any, Dict, List, Type
 
 import yaml
 from main_impt import ROOT_PATH
@@ -8,17 +8,19 @@ from systems.entity_system.base_entity import BaseEntity
 
 
 class EntityFactory:
+    __slots__ = ['_entity_registry', '_component_registry', '_existing_ids']
     
     def __init__(self):
-        self.entity_registry: Dict[str, Type[BaseEntity]] = {}
-        self.component_registry: Dict[str, Type[BaseComponent]] = {}
+        self._entity_registry: Dict[str, Type[BaseEntity]] = {}
+        self._component_registry: Dict[str, Type[BaseComponent]] = {}
+        self._existing_ids: Dict[str, List[str]] = {}
         self._register_from_yaml()
 
     def _register_entity(self, entity_type: str, entity_class: Type[BaseEntity]):
-        self.entity_registry[entity_type] = entity_class
+        self._entity_registry[entity_type] = entity_class
 
     def _register_component(self, component_type: str, component_class: Type[BaseComponent]):
-        self.component_registry[component_type] = component_class
+        self._component_registry[component_type] = component_class
 
     def _register_from_yaml(self):
         with open(os.path.join(ROOT_PATH, "Prototype", "factory_mappings.yml"), 'r', encoding="UTF-8") as file:
@@ -32,12 +34,22 @@ class EntityFactory:
 
     def _create_entity(self, entity_data: dict) -> BaseEntity:
         entity_type = entity_data['type']
-        entity_class = self.entity_registry.get(entity_type)
+        entity_class = self._entity_registry.get(entity_type)
         if not entity_class:
             raise ValueError(f"Unknown entity type: {entity_type}")
 
+        entity_id = entity_data['id']
+        
+        if entity_type not in self._existing_ids:
+            self._existing_ids[entity_type] = []
+        
+        if entity_id in self._existing_ids[entity_type]:
+            raise ValueError(f"Duplicate id {entity_id} for entity type {entity_type}")
+
+        self._existing_ids[entity_type].append(entity_id)
+
         entity = entity_class()
-        entity.id = entity_data['id']
+        entity.id = entity_id
 
         for component_data in entity_data['components']:
             component = self._create_component(component_data)
@@ -47,7 +59,7 @@ class EntityFactory:
 
     def _create_component(self, component_data: dict) -> BaseComponent:
         component_type = component_data.pop('type')
-        component_class = self.component_registry.get(component_type)
+        component_class = self._component_registry.get(component_type)
         if not component_class:
             raise ValueError(f"Unknown component type: {component_type}")
 
