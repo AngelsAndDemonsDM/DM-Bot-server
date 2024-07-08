10520de90cf5764d108455a53c2802f223b152c3
themanyfaceddemon <neko-d-05@mail.ru>
2024-07-08 14:14:41 +0300

    Блять

--- Code/main_impt.py
+++ Code/main_impt.py
@@ -1,5 +1,3 @@
 from systems.access_manager import AuthManager
-from systems.entity_factory import PrototypeFactory
 
 auth_manager = AuthManager()
-prototype_factory = PrototypeFactory()
--- Code/systems/entity_factory/__init__.py
+++
@@ -1,3 +0,0 @@
-from systems.entity_factory.base_component import Component
-from systems.entity_factory.base_entity import Entity
-from systems.entity_factory.prototype_factory import PrototypeFactory
