775c544919a667efb654ec38c1b1be92a82f3181
themanyfaceddemon <neko-d-05@mail.ru>
2024-07-10 13:35:17 +0300

    isort moment

--- Code/systems/access_manager/__init__.py
+++ Code/systems/access_manager/__init__.py
@@ -1,4 +1,2 @@
-from systems.access_manager.access_flags import (CAN_CHANGE_ACCESS,
-                                                 CAN_CHANGE_PASSWORD,
-                                                 CAN_DELETE_USERS)
+from systems.access_manager.access_flags import AccessFlags
 from systems.access_manager.auth_manager import AuthManager
