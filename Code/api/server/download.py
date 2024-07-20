cc0fca340de1162c463a61a1cc71a14244641871
themanyfaceddemon <neko-d-05@mail.ru>
2024-07-20 18:54:20 +0300

    update

--- Code/api/server/download.py
+++ Code/api/server/download.py
@@ -7,16 +7,48 @@ from quart import request, send_file
 from root_path import ROOT_PATH
 
 
-def create_zip_archive(): # TODO Проверка хеша архива
+def get_latest_modification_time(directory_path: str) -> float:
+    """Получает время последней модификации файлов в директории.
+
+    Args:
+        directory_path (str): Путь к директории.
+
+    Returns:
+        float: Время последней модификации в формате timestamp.
+    """
+    latest_time = 0
+    for root, _, files in os.walk(directory_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_time = os.path.getmtime(file_path)
+            if file_time > latest_time:
+                latest_time = file_time
+    
+    return latest_time
+
+def create_zip_archive() -> str:
+    """Создает ZIP-архив из папки 'Sprites' и возвращает путь к архиву.
+
+    Проверяет время последней модификации файлов перед пересозданием.
+
+    Returns:
+        str: Путь к созданному ZIP-архиву.
+    """
     folder_path = os.path.join(ROOT_PATH, "Sprites")
     archive_path = os.path.join(ROOT_PATH, "data", "sprites.zip")
 
-    if not os.path.exists(archive_path):
-        with zipfile.ZipFile(archive_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-            for root, dirs, files in os.walk(folder_path):
-                for file in files:
-                    file_path = os.path.join(root, file)
-                    zipf.write(file_path, os.path.relpath(file_path, folder_path))
+    directory_latest_time = get_latest_modification_time(folder_path)
+
+    if os.path.exists(archive_path):
+        archive_time = os.path.getmtime(archive_path)
+        if archive_time >= directory_latest_time:
+            return archive_path
+
+    with zipfile.ZipFile(archive_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zipf.write(file_path, os.path.relpath(file_path, folder_path))
 
     return archive_path
 
