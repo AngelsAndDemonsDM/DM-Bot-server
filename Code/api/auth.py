b7dd72b7b81ef8807b353e742e94609ead9e15b0
themanyfaceddemon <neko-d-05@mail.ru>
2024-07-24 23:38:39 +0300

    add auth api

--- Code/api/auth.py
+++ Code/api/auth.py
@@ -1,4 +1,43 @@
-from quart import Blueprint, jsonify, request, send_file
-from root_path import ROOT_PATH
+from typing import Tuple
+
+from api.decorators import server_exception_handler
+from quart import Blueprint, jsonify, request
+from systems.db_systems import UniqueConstraintError
+from systems.network import AuthError, UserAuth
 
 auth_bp = Blueprint('auth', __name__)
+
+def _get_login_password(data: dict) -> Tuple[str, str]:
+    if "login" not in data:
+        raise ValueError("Login is required")
+    
+    if "password" not in data:
+        raise ValueError("Password is required")
+    
+    return data["login"], data["password"]
+
+@server_exception_handler
+@auth_bp.route('/register', methods=['POST'])
+async def api_register():
+    user_auth = UserAuth()
+    data = await request.json
+    login, password = _get_login_password(data)
+    try:
+        await user_auth.register_user(login, password)
+        return jsonify({"message": "User registered successfully"}), 201
+    
+    except UniqueConstraintError:
+        return jsonify({"message": "Login already exists"}), 409
+
+@server_exception_handler
+@auth_bp.route('/login', methods=['POST'])
+async def api_login():
+    data = await request.json
+    login, password = _get_login_password(data)
+    user_auth = UserAuth()
+    try:
+        token = await user_auth.login_user(login, password)
+        return jsonify({"message": "Login successful", "token": token}), 200
+    
+    except AuthError:
+        return jsonify({"message": "Invalid credentials"}), 401
