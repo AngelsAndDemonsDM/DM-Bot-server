03f3a1e69ac0389467770daa7296681c67c2f5bc
themanyfaceddemon <neko-d-05@mail.ru>
2024-07-24 22:38:32 +0300

    main_impt into global_objects

--- Code/global_objects.py
+++ Code/global_objects.py
@@ -1,7 +1,8 @@
 from systems.entity_system import EntityFactory
 from systems.events import EventManager
-from systems.network import WebSocketConnectManager
+from systems.network import UserAuth, WebSocketConnectManager
 
 entity_factory = EntityFactory()
 event_manager = EventManager()
+user_auth = UserAuth()
 websocket_connect_manager = WebSocketConnectManager()
