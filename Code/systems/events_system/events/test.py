9f0534e3875712f7b6156c38469d6ce0881e0e1d
themanyfaceddemon <neko-d-05@mail.ru>
2024-07-30 23:35:10 +0300

    Вынесение регестрации в отдельный файл как на клиенте. Теперь регестрация работает по наименованию функции, а не по переменной приписанной к этой функции

--- Code/systems/events_system/events/test.py
+++ Code/systems/events_system/events/test.py
@@ -6,11 +6,9 @@ logger = logging.getLogger("Test ev")
 
 #TODO: Удалить после добавления первого нормального ивента
 
-async def test_echo(socket_user, socket_access: UserAccess, data): # Ивенты работают только по именованным аргументам! Если пользователь отправил data, то и в функцию прийдёт только data. socket_user, socket_access: UserAccess всегда можно поставить в функцию
+async def echo_event(socket_user, socket_access: UserAccess, data): # Ивенты работают только по именованным аргументам! Если пользователь отправил data, то и в функцию прийдёт только data. socket_user, socket_access: UserAccess всегда можно поставить в функцию
     logger.info(f"Echo event called with user: {socket_user}, access: {socket_access._flags}, data: {data}")
     
-    sock_con_man:SocketConnectManager = SocketConnectManager.get_instance()
+    sock_con_man:SocketConnectManager = SocketConnectManager()
     if data == "fuck u":
         await sock_con_man.send_data(socket_user, {"ev_type": "echo", "data": "fuck u 2"})
-    
-test_echo.event_name = "echo" # Указание типа ивента
