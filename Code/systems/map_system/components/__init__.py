4c752eee2ada2091a9e33d3e580adc0cf2f2ff2d
themanyfaceddemon <neko-d-05@mail.ru>
2024-08-01 16:01:59 +0300

    Регестрация компонентов

--- Code/systems/map_system/components/__init__.py
+++ Code/systems/map_system/components/__init__.py
@@ -1,1 +1,7 @@
-__all__ = []
+from systems.map_system.components.map_coordinates_component import \
+    MapCoordinatesComponent
+from systems.map_system.components.map_physics_component import \
+    MapPhysicsComponent
+from systems.map_system.components.shape_component import Shape
+
+__all__ = ['MapCoordinatesComponent', 'MapPhysicsComponent', 'Shape']
