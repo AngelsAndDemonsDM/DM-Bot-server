4e33c9a6c0d8bf0fbd77a6be739242f63c98711f
themanyfaceddemon <neko-d-05@mail.ru>
2024-08-02 17:32:27 +0300

    Добавление нормального модуля для импорта

---
+++ Code/systems/events_system/events/admin/__init__.py
@@ -0,0 +1,6 @@
+from systems.events_system.events.admin.change_access import \
+    o_admin_change_access_event
+from systems.events_system.events.admin.change_password import \
+    o_admin_change_password_event
+from systems.events_system.events.admin.delete_user import \
+    o_admin_delete_user_event
