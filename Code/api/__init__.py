8003f3f2bb4f7ca775a5e269049fcd1874e53a2d
themanyfaceddemon <neko-d-05@mail.ru>
2024-08-02 13:16:58 +0300

    Ремув аунтификации через http

--- Code/api/__init__.py
+++ Code/api/__init__.py
@@ -1,5 +1,4 @@
 from api.admin import (admin_bp, api_change_access, api_change_password,
                        api_delete_user)
-from api.auth import api_login, api_logout, api_register, auth_bp
 
-__all__ = ['admin_bp', 'auth_bp', 'server_bp', 'api_change_access', 'api_change_password', 'api_delete_user', 'api_login', 'api_logout', 'api_register', 'api_check_status', 'api_download_server_content']
+__all__ = ['admin_bp', 'api_change_access', 'api_change_password', 'api_delete_user']
