54ecceb42244e100886c438c726fac9dd9ce9a7c
themanyfaceddemon <neko-d-05@mail.ru>
2024-08-02 17:29:47 +0300

    Обновление регестрации ивентов на самостоятельное рекурсивное

--- Code/systems/events_system/events/__init__.py
+++
@@ -1,3 +0,0 @@
-from systems.events_system.events.test import echo_event
-
-__all__ = ['echo_event']
--- Code/systems/events_system/register.py
+++ Code/systems/events_system/register.py
@@ -1,27 +1,43 @@
+import importlib.util
 import logging
 import re
+from pathlib import Path
 
-from systems.events_system import events
+from root_path import ROOT_PATH
 from systems.events_system.event_manager import EventManager
 
 logger = logging.getLogger("Event manager")
+
+def import_module_from_file(module_name, file_path):
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
 
 def register_events() -> None:
     logger.info("Start register events")
     
     ev_manager = EventManager()
-    event_names = events.__all__
-    
-    event_name_pattern = re.compile(r"(.+)_event$")
-    
-    for name in event_names:
-        handler = getattr(events, name)
-        
-        if callable(handler):
-            match = event_name_pattern.match(name)
-            if match:
-                event_name = match.group(1)
-                ev_manager.register_event(event_name, handler)
-                logger.info(f"Registered event '{event_name}' with handler {handler.__name__}")
+    event_name_pattern = re.compile(r"^(?:o_|p_)?(.+?)_event$") 
+
+    events_directory = Path(ROOT_PATH)
+    events_directory = events_directory / "Code" / "systems" / "events_system" / "events"
+    for file_path in events_directory.rglob("*.py"):
+        module_name = file_path.stem  # Имя модуля без расширения .py
+        module = import_module_from_file(module_name, file_path)
+
+        for name in dir(module):
+            handler = getattr(module, name)
+            if callable(handler):
+                match = event_name_pattern.match(name)
+                if match:
+                    event_name = match.group(1)  # Извлекаем имя события без модификаторов и суффиксов
+                    if name.startswith("o_"):
+                        ev_manager.register_open_event(event_name, handler)
+                        logger.info(f"Registered open event '{event_name}' with handler '{handler.__name__}'")
+                    
+                    elif name.startswith("p_"):
+                        ev_manager.register_protected_event(event_name, handler)
+                        logger.info(f"Registered protected event '{event_name}' with handler '{handler.__name__}'")
     
     logger.info("End register events")
