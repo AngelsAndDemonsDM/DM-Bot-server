b4d863ae5ca2da23f8ffce8536b57241fb8f934e
themanyfaceddemon <neko-d-05@mail.ru>
2024-09-18 23:06:30 +0300

    f

--- Code/api/user.py
+++ Code/api/user.py
@@ -1,12 +1,11 @@
-from DMBotNetwork import ClUnit, ServerDB, Server
+from DMBotNetwork import ClUnit, ServerDB
 
 
 class UserServerModule:
     @staticmethod
-    async def net_get_access(cl_unit: ClUnit):
-        return await ServerDB.get_access(cl_unit.login)
+    async def net_get_access(cl_unit: ClUnit, login: str):
+        return await ServerDB.get_access(login)
 
     @staticmethod
     async def net_get_all_users(cl_unit: ClUnit):
-        # Блять. Нужны или произвольные запросы бд, или сделать получение существующих людей
-        return Server._cl_units.keys()
+        return await ServerDB.get_all_users()
