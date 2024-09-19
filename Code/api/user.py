18ca1e00448c60b7a956aa9df9dd7fd2322bc952
themanyfaceddemon <neko-d-05@mail.ru>
2024-09-19 12:37:50 +0300

    Однажды тебе прийдётся ответить за свои деяния

--- Code/api/user.py
+++ Code/api/user.py
@@ -1,4 +1,4 @@
-from DMBotNetwork import ClUnit, ServerDB
+from DMBotNetwork import ClUnit, ServerDB, require_access
 
 
 class UserServerModule:
@@ -9,3 +9,25 @@ class UserServerModule:
     @staticmethod
     async def net_get_all_users(cl_unit: ClUnit):
         return await ServerDB.get_all_users()
+
+    @require_access("change_access")
+    @staticmethod
+    async def net_change_access(cl_unit: ClUnit, login: str, changes: dict):
+        cur_target_access = await ServerDB.get_access(login)
+        cl_unit_access = await ServerDB.get_access(cl_unit.login)
+        
+        if cur_target_access is None or cl_unit_access is None:
+            return
+        
+        for key in changes.keys():
+            if key not in cl_unit_access:
+                raise PermissionError(key)
+            
+            if cl_unit_access[key] is False:
+                raise PermissionError(key)
+        
+        cur_target_access.update(changes)
+        
+        await ServerDB.change_user_access(login, cur_target_access)
+        
+        return "sucess"
